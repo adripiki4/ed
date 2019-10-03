97d6beb672fe78db543bf3c2a953c7b70334644a
adripiki4 <adriba@alu.iesserpis.org>
2019-10-03 09:00:38 +0200

    scanner en java2

--- JHolaMundo/src/iesserpis/ed/HolaMundo.java
+++ JHolaMundo/src/iesserpis/ed/HolaMundo.java
@@ -1,9 +1,23 @@
 package iesserpis.ed;
 
+import java.util.Scanner;
+
 public class HolaMundo {
 
 	public static void main(String[] args) {
-		System.out.println("Hola Mundo desde eclipse");
+		Scanner teclado = new Scanner(System.in);
+		String nombre;
+		int edad;
+		
+		System.out.print("Introduzca su nombre: ");
+		 nombre = teclado.nextLine();
+		System.out.print("hola, "+ nombre);
+		System.out.print("Introduzca su edad: ");
+		 edad = teclado.nextInt();
+			System.out.print("hola, "+ edad);
+
+		 
 	}
+	
 
 }
