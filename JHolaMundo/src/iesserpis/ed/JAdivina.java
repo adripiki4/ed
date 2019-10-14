44b61333dbc40d256ca63df3875eb795b29fe5fb
adripiki4 <adriba@alu.iesserpis.org>
2019-10-14 13:35:40 +0200

    añadido Jsuma mostrando while y for

---
+++ JHolaMundo/src/iesserpis/ed/JAdivina.java
@@ -0,0 +1,31 @@
+package iesserpis.ed;
+import java.util.*;
+/**
+ * 
+ * @author adriba
+ *
+ */
+public class JAdivina {
+
+	public static void main(String[] args) {
+		int contador = 1;
+		
+		while (contador <= 5) {
+			System.out.println("contador = " + contador);
+				contador = contador +1 ; //contador++
+				
+			}
+			System.out.println("Fin del programa");
+			
+		// for (int contador = 1; contador <=5; contador++)
+			// System.out.println("contador = " + contador);
+		 		// System.out.println("Fin del programa");
+	}
+		
+		
+		
+		
+
+	}
+
+
