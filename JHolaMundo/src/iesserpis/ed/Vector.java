0254d92f2d253f9b803fbba00436c60b4e6b8904
adripiki4 <adriba@alu.iesserpis.org>
2019-12-16 13:44:18 +0100

    Vector test copia no funciona

--- JHolaMundo/src/iesserpis/ed/Vector.java
+++ JHolaMundo/src/iesserpis/ed/Vector.java
@@ -4,6 +4,8 @@ public class Vector {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		int [] v1 = {14, 21, 12, 9, 7};
+		selectionSort(v1);
 		
 	}
 	public static int max(int[] v) {
@@ -16,16 +18,23 @@ public class Vector {
 		return max;
 	}
 	public static int[] selectionSort(int[] v1) {
-		for(int i=0;i< v1.length ;i++) {
-            for(int j=i+1;j<v1.length;j++){
-                if(v1[i]>v1[j]){
-                    //Intercambiamos valores
-                    int variableauxiliar=v1[i];
-                    v1[i]=v1[j];
-                    v1[j]=variableauxiliar;
-                }
-               }
-            }
-		return v1;
+//		for(int i=0;i< v1.length ;i++) {
+//            for(int j=i+1;j<v1.length;j++){
+//                if(v1[i]>v1[j]){
+//                    //Intercambiamos valores
+//                    int variableauxiliar=v1[i];
+//                    v1[i]=v1[j];
+//                    v1[j]=variableauxiliar;
+//                }
+//               }
+//            }
+		int[] copia= Arrays.copyOfRange(v1, 0, v1.length);
+		Arrays.sort(copia);
+		v1 = copia;
+		for (int i = 0; i<copia.length; i++) {
+			System.out.print(copia[i]+" ");
+		}
+		return copia;
+		
 	}
 }
