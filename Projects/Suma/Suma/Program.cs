bb2077212dda9eef3a0fecc02f3433ec27bd5dc6
adripiki4 <adriba@alu.iesserpis.org>
2020-01-13 12:59:46 +0100

    operaciones

---
+++ Projects/Suma/Suma/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Suma
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("introduzca un numero");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("introduzca un numero");
+            int b = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("la suma es: " + (a + b));
+            Console.WriteLine("la resta es: " + (a - b));
+
+            Console.WriteLine("introduzca un numero");
+            double c = double.Parse(Console.ReadLine());
+            Console.WriteLine("introduzca un numero");
+            double d = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("la multiplicacion es: " + (c * d));
+            Console.WriteLine("la division es: " + (c / d));
+
+        }
+    }
+}
