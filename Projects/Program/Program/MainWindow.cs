fa769baa89ed32ad821757b0d30ecff8213a06f1
adripiki4 <adriba@alu.iesserpis.org>
2020-01-16 08:57:28 +0100

    Introduccion a diseño grafico

---
+++ Projects/Program/Program/MainWindow.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Gtk;
+
+public partial class MainWindow : Gtk.Window
+{
+    public MainWindow() : base(Gtk.WindowType.Toplevel)
+    {
+        Build();
+    }
+
+    protected void OnDeleteEvent(object sender, DeleteEventArgs a)
+    {
+        Application.Quit();
+        a.RetVal = true;
+    }
+
+    protected void OnSaludarClicked(object sender, EventArgs e)
+    {
+        MessageDialog md = new MessageDialog(null, DialogFlags.Modal, MessageType.Error, ButtonsType.YesNo, "HOLA");
+        md.Run();
+        md.Destroy();
+    }
+
+}
